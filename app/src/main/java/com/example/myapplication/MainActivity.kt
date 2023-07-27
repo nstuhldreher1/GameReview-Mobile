acf1554a42fd3ba65d72fb0cb7f6226fc38f80a5
SirDerpsAlot <Ianpaffeldt@gmail.com>
2023-07-27 12:03:52 -0400

    Updated xml files

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -12,11 +13,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        login()
     }
 
-    fun Login() {
+    fun login() {
         val et_username = findViewById<EditText>(R.id.et_username)
         val et_password =  findViewById<EditText>(R.id.et_password)
         val btn_login = findViewById<Button>(R.id.btn_login)
+
+        btn_login.setOnClickListener {
+            val intent = Intent(this, activity_registration::class.java)
+            startActivity(intent);
+        }
+
+
     }
 }
