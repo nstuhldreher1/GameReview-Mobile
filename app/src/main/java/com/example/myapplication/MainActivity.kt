2b5e22570865609eac266e73acfc4c3e796b2ca1
Rishi Gadhia <gadhiarishi@gmail.com>
2023-07-27 18:44:17 -0400

    Added functions to send HTTP requests to each API

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -25,7 +25,40 @@ import kotlinx.serialization.json.Json
 @Serializable
 data class User(val username: String, val password: String)
 @Serializable
-data class NewUser(val username: String, val name:String, val email: String, val password: String)
+data class NewUser(val username: String, val name: String,
+    val email: String, val password: String)
+@Serializable
+data class VerifyUser(val username: String, val code: String)
+@Serializable
+data class AddReview(val userID: String, val gameID: String,
+    val rating: String, val comment: String, val gameName: String)
+@Serializable
+data class DeleteReview(val userID: String, val gameID: String)
+@Serializable
+data class EditReview(val userID: String, val gameID: String,
+    val rating: String, val comment: String)
+@Serializable
+data class RequestPassReset(val email: String)
+@Serializable
+data class VerifyOtp(val email: String, val otp: String)
+@Serializable
+data class ResetPassword(val newPassword: String, val email: String)
+@Serializable
+data class SearchGames(val searchGames: String)
+@Serializable
+data class SearchUsers(val searchUsers: String)
+@Serializable
+data class LoadGamePage(val gameID: String, val userID: String)
+
+
+
+
+
+
+
+
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -43,7 +76,7 @@ class MainActivity : AppCompatActivity() {
 
         // Call the async function using coroutines
         GlobalScope.launch {
-            connectWebsite()
+            connectWebsite("BobEvans", "wow")
         }
 
         btn_login.setOnClickListener {
@@ -52,7 +85,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    suspend fun connectWebsite() {
+    suspend fun connectWebsite(username:String, password:String) {
         val client = HttpClient(CIO) {
             install(ContentNegotiation) {
                 json(Json {
@@ -64,12 +97,251 @@ class MainActivity : AppCompatActivity() {
         }
         val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/login") {
            contentType(ContentType.Application.Json)
-            setBody(User("BobEvans", "wow"))
+            setBody(User(username, password))
 
         }
 
         println(response)
         client.close()
     }
+
+
+    suspend fun registerUser(username:String, name: String,
+                             email: String, password: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/signup") {
+            contentType(ContentType.Application.Json)
+            setBody(NewUser(username, name, email, password))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun verifyUser(username: String, code: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/verify") {
+            contentType(ContentType.Application.Json)
+            setBody(VerifyUser(username, code))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun addReview(userID: String, gameID: String,
+                          rating: String, comment: String, gameName:String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/addreview") {
+            contentType(ContentType.Application.Json)
+            setBody(AddReview(userID, gameID, rating, comment, gameName))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun deleteReview(userID: String, gameID: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/deletereview") {
+            contentType(ContentType.Application.Json)
+            setBody(DeleteReview(userID, gameID))
+
+        }
+
+        println(response)
+        client.close()
+    }
+    suspend fun editReview(userID: String, gameID: String,
+                           rating: String, comment: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/editReview") {
+            contentType(ContentType.Application.Json)
+            setBody(EditReview(userID, gameID, rating, comment))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun requestPassReset(email: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/requestPassReset") {
+            contentType(ContentType.Application.Json)
+            setBody(RequestPassReset(email))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+
+    suspend fun verifyOtp(email: String, otp: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/verifyOtp") {
+            contentType(ContentType.Application.Json)
+            setBody(VerifyOtp(email, otp))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun resetPassword(newPassword: String, email: String )
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/resetPassword") {
+            contentType(ContentType.Application.Json)
+            setBody(ResetPassword(newPassword, email))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun searchGames(searchGames: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/searchGames") {
+            contentType(ContentType.Application.Json)
+            setBody(SearchGames(searchGames))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun searchUsers(searchUsers: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/searchUsers") {
+            contentType(ContentType.Application.Json)
+            setBody(SearchUsers(searchUsers))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+    suspend fun loadGamePage(gameID: String, userID: String)
+    {
+        val client = HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    prettyPrint = true
+                    isLenient = true
+                })
+            }
+
+        }
+        val response: HttpResponse = client.post("https://gamereview-debf57bc9a85.herokuapp.com/api/verifyOtp") {
+            contentType(ContentType.Application.Json)
+            setBody(LoadGamePage(gameID, userID))
+
+        }
+
+        println(response)
+        client.close()
+    }
+
+
+
+
 
 }
